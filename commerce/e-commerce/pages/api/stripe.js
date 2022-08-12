07ad4e11fe1b4c8a55dd3ab45f3976737a2aff4b
pyloverm <ferreirabictor74200@gmail.com>
2022-08-12 12:12:40 +0100

    routing update

--- commerce/e-commerce/pages/api/stripe.js
+++ commerce/e-commerce/pages/api/stripe.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       const lang = req.headers.locale;
       var lang_url = '/'.concat(lang);
       if (lang === 'en'){
-        lang_url = ''
+        lang_url = '/en-US'
       }
       const params = {
         submit_type: 'pay',
